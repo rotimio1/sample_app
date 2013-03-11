8c868f16757c2f6ede571478eb63d28a13d558f7
Rotimio1 <rotimio1@umbc.edu>
2013-03-11 01:42:13 -0400

    Finish exercices

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,37 +1,23 @@
 require 'spec_helper'
-describe "Static pages" do	
-subject { page }
-shared_examples_for "all static pages" do
-it { should have_selector('h1',    text: heading) }
-it { should have_selector('title', text: full_title(page_title)) }
+describe "Static pages" do
+it "should have the right links on the layout" do
+visit root_path
+
+click_link "About"
+page.should have_selector 'title', text: full_title('About Us')
+
+click_link "Help"
+page.should have_selector 'title', text: full_title('Help')
+
+click_link "Contact"
+page.should have_selector 'title', text: full_title('contact')
+
+click_link "Sign up now!"
+page.should have_selector 'title', text: full_title('sign up')
+
+click_link "sample app"
+page.should have_selector 'title', text: full_title('Home')
+
 end
 
-describe "Home page" do
-before { visit root_path }
-let(:heading)    { 'Sample App' }
-let(:page_title) { '' }
-it_should_behave_like "all static pages"
-it { should_not have_selector 'title', text: '| Home' }
-end
-
-describe "Help page" do
-before { visit root_path }
-let(:heading)    { '' }
-let(:page_title) { '' }
-it_should_behave_like "all static pages"
-end
-
-describe "About page" do
-before { visit root_path }
-let(:heading)    { '' }
-let(:page_title) { '' }
-it_should_behave_like "all static pages"
-end
-
-describe "Contact page" do
-before { visit root_path }
-let(:heading)    { '' }
-let(:page_title) { '' }
-it_should_behave_like "all static pages"
-end
 end
