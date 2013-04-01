e7794c8e3429a8d8b25c62fa5f99d9357c0acebe
Rotimio1 <rotimio1@umbc.edu>
2013-04-01 07:24:34 -0400

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,5 @@
 # == Schema Information
-#
-# Table name: users
-#
-#  id              :integer          not null, primary key
-#  name            :string(255)
-#  email           :string(255)
-#  created_at      :datetime         not null
-#  updated_at      :datetime         not null
-#  password_digest :string(255)
-#
-
+## Schema version: 20130323012536
 # Table name: users
 #
 #  id         :integer          not null, primary key
@@ -28,6 +18,6 @@ VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 validates :email, presence: true,
 format: { with: VALID_EMAIL_REGEX },
 uniqueness: { case_sensitive: false }
-validates :password, presence: true, length: { minimum: 6 }
+validates :password, length: { minimum: 6 }
 validates :password_confirmation, presence: true
 end
