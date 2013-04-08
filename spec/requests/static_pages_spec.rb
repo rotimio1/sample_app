cbb49407feec0c7e5754ee6685036647102a4134
Rotimio1 <rotimio1@umbc.edu>
2013-04-07 23:23:25 -0400

    Finish sign in

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,23 +1,52 @@
 require 'spec_helper'
+
 describe "Static pages" do
-it "should have the right links on the layout" do
-visit root_path
 
-click_link "About"
-page.should have_selector 'title', text: full_title('About Us')
+ subject { page }
 
-click_link "Help"
-page.should have_selector 'title', text: full_title('Help')
+ describe "Home page" do
+   before { visit root_path }	
 
-click_link "Contact"
-page.should have_selector 'title', text: full_title('contact')
+   it { should have_selector('h1', text: 'Sample App') }
+   it { should have_selector('title', text: full_title('')) }
+   it { should_not have_selector('title', text: '| Home') }
+  end
 
-click_link "Sign up now!"
-page.should have_selector 'title', text: full_title('sign up')
+ describe "Help page" do
+  before { visit help_path }
 
-click_link "sample app"
-page.should have_selector 'title', text: full_title('Home')
+  it { should have_selector('h1', text: 'Help') }
+  it { should have_selector('title', text: full_title('Help')) }
+ end
 
-end
+ describe "About page" do
+  before { visit about_path }
 
+  it { should have_selector('h1', text: 'About Us') }
+  it { should have_selector('title', text: full_title('About Us')) }
+ end
+
+ describe "Contact page" do
+  before { visit contact_path }
+
+  it { should have_selector('h1', text: 'Contact') }
+  it { should have_selector('title', text: full_title('Contact')) }
+ end
+
+ it "should have the right links on the layout" do
+  visit root_path
+  click_link "Sign in"
+  page.should have_selector 'title'|
+  click_link "About"
+  page.should have_selector 'title', text: full_title('About Us')
+  click_link "Help"
+  page.should have_selector 'title', text: full_title('Help')
+  click_link "Contact"
+  page.should have_selector 'title', text: full_title('contact')
+  click_link "Home"
+  click_link "Sign up now!"
+  page.should have_selector 'title', text: full_title('sign up')
+  click_link "sample app"
+  page.should have_selector 'title', text: full_title('sample App')
+ end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,9 +1,18 @@
 require 'spec_helper'
-describe "User pages" do
-  subject { page }
 
+describe "User pages" do
+  
+  subject { page }
+I
+describe "signup page" do
+     before { visit signup_path }
+
+     it { should have_selector('h1',    text: 'Sign up') }
+     it { should have_selector('title', text: full_title('Sign up')) }
+  end 
+  
   describe "profile page" do
-    let(:user) { FactoryGirl.create(:user) }
+      let(:user) { FactoryGirl.create(:user) }
 
     before { visit user_path(user) }
 
@@ -13,6 +22,7 @@ describe "User pages" do
 
   describe "signup page" do
      before { visit signup_path }
+  
      let(:submit) { "Create my account" }
    
     describe "with invalid information" do
@@ -22,11 +32,15 @@ describe "User pages" do
 
    describe "after submission" do
      before { click_button submit }
+
      it { should have_selector('title', text: 'Sign up') }
      it { should have_content('error') }
+     it { should _not have_content('Password digest') }
+    end
   end
 
    describe "with valid information" do
+
     before do
   	fill_in "Name",         with: "Example User"
     fill_in "Email",        with: "user@example.com" 
@@ -38,15 +52,16 @@ describe "User pages" do
     expect { click_button submit }.to change(User, :count).by(1)
   end
 
-  describe "after saving the user" do
-  before { click_button submit }
-  let(:user) { User.find_by_email('user@example.com') }
-  it { should have_selector('title', text: user.name) }
-  it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+  describe "after saving a user" do
+    before { click_button submit }
+   
+    let(:user) { User.find_by_email('user@example.com') }
+  
+    it { should have_selector('title', text: user.name) }
+    it { should have_selector('div.alert.alert-success', text: 'Welcome') }
+    it { should have_link('Sign out') }
+   end
+  end
  end
-
-end
-end
-end
 end
 
